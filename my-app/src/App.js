7ceceba45257496905d821391fd378c107e8d7a0
hangcheng151 <547299546@qq.com>
2019-12-27 15:05:57 +0800

    moban

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './style.less';
+import React, { Component } from 'react'
+
+ class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    )
+  }
+}
+
+
+export default App;
